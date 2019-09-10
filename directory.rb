14f4782897f5580a1f069880374ebd69a771ab2c
Josh Davies <jdaviescreative@gmail.com>
2019-09-10 17:52:44 +0000

    total number of students

--- directory.rb
+++ directory.rb
@@ -11,3 +11,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#finally, print the total number of students
+print "Overall, we have "
+#print() doesn't add new line
+print 11
+puts " great students"
