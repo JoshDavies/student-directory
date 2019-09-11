aa2d915511afe4fd16c8c0e7548622d551cd8ef8
Josh Davies <jdaviescreative@gmail.com>
2019-09-11 17:21:41 +0000

    exercise 4 and 5, using a while loop and adding date of birth

--- exercise4.rb
+++ exercise4.rb
@@ -1,12 +1,16 @@
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "To finish, just hit return thrice"
   students = []
   name = gets.chomp
+  puts "Now add their Date of Birth:"
+  country_of_birth = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, cob: country_of_birth}
     puts "Now we have #{students.count} students"
     name = gets.chomp
+    puts "Now add their Date of Birth:"
+    country_of_birth = gets.chomp
   end
   students
 end
@@ -15,11 +19,13 @@ def print_header
   puts "The students of Villains Academy"
   puts "-----------"
 end
-
+# Using a while method to print
 def print(students)
   count = 0
   while count < students.length do
-      puts "#{count +1}. #{students[0 + count][:name]} (#{students[0 + count][:cohort]} cohort)"
+      puts "#{count +1}. #{students[0 + count][:name]} 
+      (#{students[0 + count][:cohort]} cohort)
+      (D.O.B: #{students[0 + count][:cob]})"
     count += 1
   end
 end
