b127b34e1f9b62589028532c9b3d4a8ecb1597d3
Josh Davies <jdaviescreative@gmail.com>
2019-09-14 19:40:03 +0000

    Refactored by add_students

--- directory.rb
+++ directory.rb
@@ -36,11 +36,19 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   name = STDIN.gets.chomp
+  puts "Enter their cohort"
+  cohort = STDIN.gets.chomp
   while !name.empty? do
-    @students << {name: name, cohort: :november}
+    add_students(name, cohort)
     puts "Now we have #{@students.count} students"
     name = STDIN.gets.chomp
+    puts "Enter their cohort"
+    cohort = STDIN.gets.chomp
   end
+end
+
+def add_students(name, cohort)
+  @students << {name: name, cohort: cohort.to_sym}
 end
 
 def show_students
@@ -77,7 +85,7 @@ def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
+  add_students(name, cohort)
   end
   file.close
 end
